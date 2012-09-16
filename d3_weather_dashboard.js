56d53af6a0eeafabab5f3f01b0a808bd67bb999d
Andrew Nelson <werdnanoslen@risu.(none)>
2012-09-16 14:23:00 -0400

    gui ready, weatherunderground API initial

--- d3_weather_dashboard.js
+++ d3_weather_dashboard.js
@@ -1,16 +1,63 @@
-API_KEY = "3ac9e3b42d9cdee2";
+const API_KEY = "3ac9e3b42d9cdee2";
 
-var url = "http://api.wunderground.com/api/"
-		+API_KEY
-		+"/conditions/tide/hourly10day/q/"
-		+SEARCH_LOCATION
-		+".json";
-$.ajax
-({
-	url: url,
-	dataType: "jsonp",
-	success: function(weatherData) 
-	{
-		// use the JSON data here for your visualization
-	}
-});
+var city;
+var day;
+var $dayEl;
+var inputTimer;
+var weatherReport;
+
+window.onload = initialize;
+
+function initialize()
+{
+    setCity($("#city"));
+    setDay($("#dayToday"));
+}
+
+function setCity($newCityEl)
+{
+    window.clearTimeout(inputTimer);
+    inputTimer = window.setTimeout //wait for entire word to be inputted to cut down on load
+    (
+        function()
+        {
+            city = $($newCityEl).val();
+            getWeatherReport();
+        },
+        1000
+    )
+}
+
+function setDay($newDayEl)
+{
+    $($dayEl).removeClass("buttonSelected");
+    $dayEl = $($newDayEl);
+    day = $dayEl.text(); //how do I capture dates?
+    $dayEl.addClass("buttonSelected");
+    getWeatherReport();
+}
+
+function getWeatherReport()
+{
+    var url = "http://api.wunderground.com/api/"
+        +API_KEY
+        +"/conditions/tide/hourly10day/q/"
+        +city
+        +".json";
+    $.ajax
+        ({
+            url: url,
+            dataType: "jsonp",
+            success: function(weatherData) 
+            {
+                weatherReport = jQuery.parseJSON(weatherData);
+                updateUI();
+            }
+        });
+}
+
+function updateUI()
+{
+    alert("");
+}
+
