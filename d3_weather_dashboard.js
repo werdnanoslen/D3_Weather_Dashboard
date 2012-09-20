993d679ce006900a00346cb376c2f7c9b35552c1
Andrew Nelson <werdnanoslen@gmail.com>
2012-09-19 21:57:16 -0400

    finally got wunderground request to work

--- d3_weather_dashboard.js
+++ d3_weather_dashboard.js
@@ -1,6 +1,7 @@
-const API_KEY = "3ac9e3b42d9cdee2";
+const API_KEY = "81adde7daaaf8da5";
 
 var city;
+var newCity;
 var day;
 var $dayEl;
 var inputTimer;
@@ -17,47 +18,64 @@ function initialize()
 function setCity($newCityEl)
 {
     window.clearTimeout(inputTimer);
-    inputTimer = window.setTimeout //wait for entire word to be inputted to cut down on load
-    (
-        function()
-        {
-            city = $($newCityEl).val();
-            getWeatherReport();
-        },
-        1000
-    )
+    inputTimer = window.setTimeout
+	(
+		function()
+		{
+			city = $($newCityEl).val();
+			getWeatherReport();
+		},
+		1000
+	)
 }
 
 function setDay($newDayEl)
 {
-    $($dayEl).removeClass("buttonSelected");
-    $dayEl = $($newDayEl);
+    $($dayEl).removeClass("buttonSelected"); //clear selected button
+    $dayEl = $($newDayEl); //set new selected button
     day = $dayEl.text(); //how do I capture dates?
-    $dayEl.addClass("buttonSelected");
+    $dayEl.addClass("buttonSelected"); //display button selection
     getWeatherReport();
 }
 
 function getWeatherReport()
 {
+    /*$.ajax
+    ({
+        url: "http://autocomplete.wunderground.com/aq?query="+city, 
+        type: "GET",
+        dataType: "jsonp",
+        success: function(data)
+        {
+            city = data[0]['l'];
+			alert(city);
+        }
+    });*/
+	
+	var newCity = $.getJSON("http://autocomplete.wunderground.com/aq?query="+city);
+	document.write(JSON.stringify(newCity));
+	
+
     var url = "http://api.wunderground.com/api/"
         +API_KEY
-        +"/conditions/tide/hourly10day/q/"
+        +"/hourly10day/q/"
         +city
         +".json";
     $.ajax
-        ({
-            url: url,
-            dataType: "jsonp",
-            success: function(weatherData) 
-            {
-                weatherReport = jQuery.parseJSON(weatherData);
-                updateUI();
-            }
-        });
+    ({
+        url: url,
+        dataType: "jsonp",
+        success: function(weatherData) 
+        {
+            weatherReport = weatherData;
+            updateUI();
+        }
+    });
 }
 
 function updateUI()
 {
-    alert("");
+    //alert(weatherReport['hourly_forecast'][0]['temp']['metric']);
+    //alert(weatherReport['FCTTIME']['pretty']);
 }
 
